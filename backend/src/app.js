8aafaafe73c3c069eb4d82f21bba0b037e8d9431
Maruf Ajagunna <marufajagunna@gmail.com>
2021-04-08 17:48:05 +0100

    swagger review

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes/index');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./config/swagger')
 const cors = require('cors')
 
 const app = express();
@@ -8,10 +10,15 @@ const app = express();
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cors());
 // parse application/json
 app.use(bodyParser.json());
 
+app.use(cors());
+
+// use swagger-Ui-express for your app documentation endpoint
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// Routes for the end-points
 app.use('/api', routes);
 
 module.exports = app;
